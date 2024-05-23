a67b93913f75608cb5456b2fc0dae51c01a76393
ahmedrazarais <razarais28@gmail.com>
2024-05-23 20:04:38 +0500

    add to repo

---
+++ Largest-substring.py
@@ -0,0 +1,25 @@
+# Given a string s, find the length of the longest 
+# substring
+#  without repeating characters.
+
+
+
+def lengthOfLongestSubstring(s):
+        char_set = set()
+        left = 0
+        max_length = 0
+        
+        for right in range(len(s)):
+            while s[right] in char_set:
+                char_set.remove(s[left])
+                left += 1
+            char_set.add(s[right])
+            max_length = max(max_length, right - left + 1)
+        
+        return max_length
+
+
+s3 = "pwwkew"
+print(lengthOfLongestSubstring(s3))
+
+            
